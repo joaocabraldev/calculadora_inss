536b1d9099e78d48b51cae0998775138304d1052
Joao Antonio Cabral <joaomunizshura@gmail.com>
2020-01-25 13:31:33 -0300

    Java Lint

--- calculadora_inss_java/src/main/java/br/com/joaocabral/calculadora_inss_java/service/CalculoService.java
+++ calculadora_inss_java/src/main/java/br/com/joaocabral/calculadora_inss_java/service/CalculoService.java
@@ -4,7 +4,6 @@ import br.com.joaocabral.calculadora_inss_java.model.Faixa;
 import br.com.joaocabral.calculadora_inss_java.model.Resultado;
 import java.util.ArrayList;
 import java.util.List;
-import jdk.nashorn.internal.objects.NativeArray;
 
 /**
  * Serviço de Cálculos.
@@ -19,43 +18,43 @@ public class CalculoService {
     }
     
     
-    public Faixa selecionarFaixa(Double salario) {
+    public Faixa selecionarFaixa(final Double salario) {
         Faixa faixaSelecionada = null;
-        
-        for (Faixa faixa : FAIXASANTIGAS) {
-            if(salario >= faixa.vinicial && salario <= faixa.vfinal) {
+
+        for (final Faixa faixa : FAIXASANTIGAS) {
+            if (salario >= faixa.vinicial && salario <= faixa.vfinal) {
                 faixaSelecionada = faixa;
             }
         }
-        
+
         return faixaSelecionada;
     }
-    
-    public Resultado calcularINSS(Double salario) {
-        Resultado resultado = new Resultado();
-        Faixa faixa = selecionarFaixa(salario);
-        
-        if(faixa.numero < 4) {
+
+    public Resultado calcularINSS(final Double salario) {
+        final Resultado resultado = new Resultado();
+        final Faixa faixa = selecionarFaixa(salario);
+
+        if (faixa.numero < 4) {
             resultado.salario = salario;
             resultado.valor = salario * faixa.aliquota / 100;
         } else {
             resultado.salario = faixa.vinicial;
             resultado.valor = faixa.vinicial * faixa.aliquota / 100;
         }
-        
+
         resultado.aliquota = faixa.aliquota;
         resultado.faixa = faixa.numero;
-        
+
         return resultado;
     }
-    
-    public Double preparaSalarioCalculo(String salario) {
-        String salarioPreparado = salario.replace(",", ".");
+
+    public Double preparaSalarioCalculo(final String salario) {
+        final String salarioPreparado = salario.replace(",", ".");
         return Double.valueOf(salarioPreparado);
-    } 
-    
+    }
+
     private List<Faixa> preencheFaixas() {
-        List<Faixa> faixas = new ArrayList<>();
+        final List<Faixa> faixas = new ArrayList<>();
         
         faixas.add(new Faixa(0.0, 1830.30, 8.0, 1));
         faixas.add(new Faixa(1830.31, 3050.53, 9.0, 2));
