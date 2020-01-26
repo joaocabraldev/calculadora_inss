e584fb7bbc7a69ec56e213f60a677b68cddbc5d4
Joao Antonio Cabral <joaomunizshura@gmail.com>
2020-01-26 18:12:12 -0300

    Cálculo INSS Novo do Teto

--- calculadora_inss_electron/src/js/calculo.js
+++ calculadora_inss_electron/src/js/calculo.js
@@ -5,26 +5,26 @@ const calculo = {
         faixa: 0,
         valor: 0,
     },
-    
+
     faixas: [
-        [ 
-            { 
-                vinicial: 0, 
-                vfinal: 1830.30, 
+        [
+            {
+                vinicial: 0,
+                vfinal: 1830.30,
                 aliquota: 8,
                 numero: 1
-            }, { 
-                vinicial: 1830.31, 
-                vfinal: 3050.53, 
+            }, {
+                vinicial: 1830.31,
+                vfinal: 3050.53,
                 aliquota: 9,
                 numero: 2
-            },{ 
-                vinicial: 3050.54, 
-                vfinal: 6101.06, 
+            },{
+                vinicial: 3050.54,
+                vfinal: 6101.06,
                 aliquota: 11,
                 numero: 3
             },
-            { 
+            {
                 vinicial: 6101.07,
                 vfinal: 99999999,
                 aliquota: 11,
@@ -32,22 +32,22 @@ const calculo = {
             }
         ],
         [
-            { 
+            {
                 vinicial: 0,
                 vfinal: 1039.00,
                 aliquota: 7.5,
                 numero: 1
-            }, { 
+            }, {
                 vinicial: 1039.01,
                 vfinal: 2089.60,
                 aliquota: 9,
                 numero: 2
-            }, { 
+            }, {
                 vinicial: 2089.61,
                 vfinal: 3134.40,
                 aliquota: 12,
                 numero: 3
-            }, { 
+            }, {
                 vinicial: 3134.40,
                 vfinal: 6101.06,
                 aliquota: 14,
@@ -60,7 +60,7 @@ const calculo = {
         let faixaSelecionada = 0;
 
         calculo.faixas[0].forEach(faixa => {
-            if (salario >= faixa.vinicial 
+            if (salario >= faixa.vinicial
                     && salario <= faixa.vfinal) {
                 faixaSelecionada = faixa;
             }
@@ -73,7 +73,7 @@ const calculo = {
         let faixaSelecionada = 0;
 
         calculo.faixas[1].forEach(faixa => {
-            if (salario >= faixa.vinicial 
+            if (salario >= faixa.vinicial
                     && salario <= faixa.vfinal) {
                 faixaSelecionada = faixa;
             }
@@ -83,7 +83,7 @@ const calculo = {
     },
 
     preparaValorCalculo: (valor) => {
-        return valor.replace(',', '.');    
+        return valor.replace(',', '.');
     },
 
     calculaAliquotaReal: (salario, valor) => {
@@ -101,9 +101,9 @@ const calculo = {
         calculo.limpaValores();
         const salarioPreparado = calculo.preparaValorCalculo(salario);
         calculo.resultado.salario = salarioPreparado;
-    
+
         const faixa = calculo.selecionaFaixa(salarioPreparado);
-    
+
         if (faixa.numero < 4) {
             calculo.resultado.valor = salarioPreparado * faixa.aliquota / 100;
             calculo.resultado.aliquota = faixa.aliquota;
@@ -114,27 +114,32 @@ const calculo = {
             calculo.resultado.aliquota = faixa.aliquota;
             calculo.resultado.faixa = faixa.numero;
         }
-    
+
         return calculo.resultado;
     },
 
     calculaNovoINSS: (salario) => {
         calculo.limpaValores();
+
+        if (salario > calculo.faixas[1][3].vfinal) {
+            salario = calculo.faixas[1][3].vfinal;
+        }
+
         const faixaSelecionada = calculo.selecionaFaixaNova(salario);
-        
+
         calculo.faixas[1].forEach(faixa => {
             if (faixa.numero <= faixaSelecionada.numero) {
                 let diferenca = 0;
-                if (salario > faixa.vfinal) {
+                if (salario > faixa.vfinal && faixa.numero < 5) {
                     diferenca = faixa.vfinal - faixa.vinicial;
                 } else {
                     diferenca = salario - faixa.vinicial;
                 }
                 calculo.resultado.valor += diferenca * faixa.aliquota / 100;
-                
+
                 calculo.resultado.salario = salario;
                 calculo.resultado.aliquota = calculo
-                                                .calculaAliquotaReal(salario, 
+                                                .calculaAliquotaReal(salario,
                                                 calculo.resultado.valor);
                 calculo.resultado.faixa = faixa.numero;
             }
